b6b6e983e89af781d24239c0631354b91c5fd39c
ahmed.motawea <ll002415@rn-office.com>
2024-05-14 11:57:30 +0300

    add header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-
+    HeaderComponent
   ],
   imports: [
     CommonModule,
@@ -20,6 +21,7 @@ import { TranslateModule } from '@ngx-translate/core';
     FormsModule,
     RouterModule,
     TranslateModule,
+    HeaderComponent
   ],
 })
 export class SharedModule {}
