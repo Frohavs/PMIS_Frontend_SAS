80f88842b49691ddd44da22bd10c405d58deded5
amotawe3 <Motawea@frohavs.com>
2024-07-31 11:13:52 +0300

    add project boq

--- src/app/modules/project/overview/project-options.ts
+++ src/app/modules/project/overview/project-options.ts
@@ -2,5 +2,9 @@ export const PROJECT_OPTIONS = [
   {
     name: 'update project information',
     route: 'projects/update-project-info'
+  },
+  {
+    name: 'add project boq',
+    route: 'projects/add-boq'
   }
 ]
