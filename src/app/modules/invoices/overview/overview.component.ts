90835a846b348901988466967e321308cbc4805b
amotawe3 <Motawea@frohavs.com>
2024-09-03 18:33:03 +0300

    add invoices table

--- src/app/modules/invoices/overview/overview.component.ts
+++ src/app/modules/invoices/overview/overview.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateService } from '@ngx-translate/core';
+import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
+import { fromEvent, debounceTime, distinctUntilChanged, Subscription } from 'rxjs';
+import { SweetAlertOptions } from 'sweetalert2';
 
 @Component({
   selector: 'app-overview',
@@ -8,13 +14,47 @@ import { Component, OnInit } from '@angular/core';
 export class OverviewComponent implements OnInit {
 
   activeTab: number = 0;
+  etimadNumber: string = '190301099926';
+  Add_text: string;
+  Search_text: string;
+  dataList: any[] = []
+  totalCount: number;
+  pagesCount: number[] = [];
+  selected = 1;
 
-  etimadNumber: string;
+  // modal configs
+  isLoading = false;
+  isCollapsed1 = false;
+  swalOptions: SweetAlertOptions = { buttonsStyling: false };
+  @ViewChild('noticeSwal') noticeSwal!: SwalComponent;
 
-  constructor() { }
+  modalConfig: NgbModalOptions = {
+    modalDialogClass: 'modal-dialog modal-dialog-centered mw-650px',
+  };
+  private inputSubscription: Subscription;
+
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private router: Router,
+    private elRef: ElementRef,
+    private modalService: NgbModal,
+    private translate: TranslateService,
+  ) { }
 
   ngOnInit(): void {
 
+  }
+
+  ngAfterViewInit(): void {
+    const inputElement = this.elRef.nativeElement.querySelector('input[data-action="filter"]');
+
+    this.inputSubscription = fromEvent(inputElement, 'input').pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+    ).subscribe((event: any) => {
+      const searchText = event.target.value;
+      // this.initializeProjectData(1, '')
+    });
   }
   setActiveTab(index: number) {
     this.activeTab = index;
@@ -26,5 +66,44 @@ export class OverviewComponent implements OnInit {
       return false;
     }
     return true;
+  }
+
+  navigatePage(pageIndex: number) {
+    this.selected = pageIndex;
+    // this.initializeProjectData(pageIndex, '');
+  }
+
+  navigateArrows(next: boolean) {
+    if(next) {
+      if (this.selected === this.pagesCount.length) {
+        return;
+      } else {
+        this.selected += 1;
+        // this.initializeProjectData(this.selected);
+      }
+    } else {
+      if (this.selected === 1) {
+        return;
+      } else {
+        this.selected -= 1;
+        // this.initializeProjectData(this.selected);
+      }
+    }
+  }
+
+  showAlert(swalOptions: SweetAlertOptions) {
+    let style = swalOptions.icon?.toString() || 'success';
+    if (swalOptions.icon === 'error') {
+      style = 'danger';
+    }
+    this.swalOptions = Object.assign({
+      buttonsStyling: false,
+      confirmButtonText: "Ok, got it!",
+      customClass: {
+        confirmButton: "btn btn-" + style
+      }
+    }, swalOptions);
+    this.cdr.detectChanges();
+    this.noticeSwal.fire();
   }
 }
