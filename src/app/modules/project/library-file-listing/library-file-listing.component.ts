5f160bdd36283b0ed2596ba8d5a3418a0de13ed1
amotawe3 <Motawea@frohavs.com>
2024-11-22 01:54:28 +0200

    fetch tree list from api, create standard service

--- src/app/modules/project/library-file-listing/library-file-listing.component.ts
+++ src/app/modules/project/library-file-listing/library-file-listing.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from 'express';
+import { StandardTreeService } from 'src/app/services/standard-tree.service';
 
 @Component({
   selector: 'app-library-file-listing',
   templateUrl: './library-file-listing.component.html',
   styleUrl: './library-file-listing.component.scss'
 })
-export class LibraryFileListingComponent {
+export class LibraryFileListingComponent implements OnInit {
 
+  projectId!: number;
+  treeData: any[] = [];
+
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private activatedRoute: ActivatedRoute,
+    private standardTreeService: StandardTreeService
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.projectId = +params['id'];
+      if (this.projectId) {
+        this.standardTreeService.getByPRojectId(this.projectId).subscribe(res => {
+          this.treeData = res.data;
+          this.cdr.detectChanges();
+        });
+      }
+    })
+  }
+
+  toggleNode(node: any): void {
+    node.expanded = !node.expanded;
+  }
 }
---
+++ src/app/services/standard-tree.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StandardTreeService {
+
+  API_USERS_URL = `${environment.apiUrl}/Standard`;
+  token: string;
+  private authLocalStorageToken = `${environment.appVersion}-${environment.USERDATA_KEY}`;
+
+  constructor(private http: HttpClient) {
+    const lsValue = localStorage.getItem(this.authLocalStorageToken);
+    this.token = JSON.parse(lsValue as any)?.token;
+  }
+
+  // public methods
+  getByPRojectId(id: number,): Observable<any> {
+    const httpHeaders = new HttpHeaders({
+      Authorization: `Bearer ${this.token}`,
+    });
+
+    const url = `${this.API_USERS_URL}/Get?projectId=${id}`;
+    return this.http.get<any>(url, {
+      headers: httpHeaders
+    });
+  }
+}
